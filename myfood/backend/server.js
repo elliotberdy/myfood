36b4980282f037051a244df12a1b41caf934d663
Elliot Berdy <berdyelliot@gmail.com>
2023-09-20 12:28:21 -0700

    missing ;:

--- myfood/backend/server.js
+++ myfood/backend/server.js
@@ -1,4 +1,4 @@
-const sqlite3 = require('sqlite3').verbose()
+const sqlite3 = require('sqlite3').verbose();
 const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
