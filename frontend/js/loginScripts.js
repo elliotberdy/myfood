cd3ecb26ef004ed7e9e579c01b2d6455c526aa1b
Elliot Berdy <berdyelliot@gmail.com>
2023-10-04 11:49:38 -0700

    username/password required

--- frontend/js/loginScripts.js
+++ frontend/js/loginScripts.js
@@ -1,19 +1,36 @@
 // loginScipts.js
 
 document.addEventListener("DOMContentLoaded", function () {
-    const loginForm = document.getElementById("login-form");
-  
-    loginForm.addEventListener("submit", function (event) {
-      event.preventDefault(); // Prevent the default form submission
-  
-      // Get the username and password from the input fields
-      const username = document.getElementById("typeEmailX").value;
-      const password = document.getElementById("typePasswordX").value;
+    'use strict';
 
-      // Validate that both fields are not empty
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation');
+
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+        .forEach(function (form) {
+            form.addEventListener('submit', function (event) {
+                if (!form.checkValidity()) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+
+                form.classList.add('was-validated');
+            }, false);
+        });
+
+    const loginForm = document.getElementById("login-form");
+
+    loginForm.addEventListener("submit", function (event) {
+        event.preventDefault(); // Prevent the default form submission
+
+        // Get the username and password from the input fields
+        const username = document.getElementById("typeEmailX").value;
+        const password = document.getElementById("typePasswordX").value;
+
+        // Check if either username or password is empty
         if (!username || !password) {
-            alert("Please enter both a username and a password.");
-            return; // Prevent further execution
+            return; // Don't proceed with the fetch request
         }
 
         fetch('/login', {
@@ -28,7 +45,7 @@ document.addEventListener("DOMContentLoaded", function () {
             if (data.success) {
                 // Extract the userID from the response
                 const userid = data.userID;
-        
+
                 // Construct the URL with the userid and redirect
                 const redirectUrl = `/index/index.html?userid=${userid}`;
                 window.location.href = redirectUrl;
@@ -40,7 +57,6 @@ document.addEventListener("DOMContentLoaded", function () {
             console.error('Error:', error);
             alert('An error occurred. Please try again later.');
         });
-          
+
     });
 });
-
